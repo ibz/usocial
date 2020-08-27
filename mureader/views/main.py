619f712b77ffee1520329497e79ced13d6911481
Ionuț Bizău <i@ibz.me>
2020-08-27 11:02:10 +0800

    About page.

--- mureader/views/main.py
+++ mureader/views/main.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, render_template, url_for
+from flask_jwt_extended import current_user, verify_jwt_in_request
+from flask_jwt_extended.exceptions import NoAuthorizationError
 
 main_blueprint = Blueprint('main', __name__)
 
 @main_blueprint.route('/', methods=['GET'])
 def index():
     return redirect(url_for('feed.news'))
+
+@main_blueprint.route('/about', methods=['GET'])
+def about():
+    try:
+        verify_jwt_in_request()
+    except NoAuthorizationError:
+        pass
+    return render_template('about.html', user=current_user)
