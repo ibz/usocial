25c9b97114546011dcc33c73d07a07bf58054592
Ionuț Bizău <i@ibz.me>
2020-11-02 15:23:27 +0800

    Add robots.txt & sitemap

--- musocial/views/main.py
+++ musocial/views/main.py
@@ -1,8 +1,22 @@
-from flask import Blueprint, redirect, render_template, url_for
+from flask import Blueprint, redirect, render_template, Response, url_for
 from flask_jwt_extended import current_user, verify_jwt_in_request
 from flask_jwt_extended.exceptions import NoAuthorizationError
 
 main_blueprint = Blueprint('main', __name__)
+
+@main_blueprint.route('/sitemap.xml', methods=['GET'])
+def sitemap_xml():
+    return Response("""<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            <url><loc>http://musocial.me/about</loc></url>
+        </urlset>""", mimetype='application/xml')
+
+@main_blueprint.route('/robots.txt', methods=['GET'])
+def robots_txt():
+    return Response("""User-agent: *
+Allow: /about
+Disallow: /
+""", mimetype='text/plain')
 
 @main_blueprint.route('/', methods=['GET'])
 def index():
